45975885a5655adfbb6a79c2380244d2c1e742b3
Tetsuhiro Koyama <te-koyama@usen.co.jp>
2018-03-28 15:50:21 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 test -f /opt/bin/docker-compose && exit
 sudo mkdir -p /opt/bin/
-sudo curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /opt/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > ./docker-compose
+sudo mv docker-compose /opt/bin/.
 sudo chmod +x /opt/bin/docker-compose
 docker-compose -v
 
